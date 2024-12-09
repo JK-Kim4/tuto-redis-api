72a1f13b16276b35afb8fee65c4e605069494a2c
jongwan <jongbell4@gmail.com>
2024-12-09 20:25:10 +0900

    feat: redis climbing api

---
+++ src/main/java/com/tutomato/climbinggymapi/ClimbingGymApiApplication.java
@@ -0,0 +1,13 @@
+package com.tutomato.climbinggymapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbingGymApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimbingGymApiApplication.class, args);
+    }
+
+}
