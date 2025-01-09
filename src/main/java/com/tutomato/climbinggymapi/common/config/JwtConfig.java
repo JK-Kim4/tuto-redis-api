3a344636650fad177ad6e7b41ff2854511a37a01
jongwankim <jongbell4@gmail.com>
2025-01-09 13:54:32 +0900

    feat: jwt with / without

---
+++ src/main/java/com/tutomato/climbinggymapi/common/config/JwtConfig.java
@@ -0,0 +1,10 @@
+package com.tutomato.climbinggymapi.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class JwtConfig {
+}
